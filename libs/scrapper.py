9456f580dc0358583fe2e232c71c191a71d5c132
Sebastián Hernández <seba.hg@me.com>
2020-06-26 01:49:51 -0400

    refactor(scrapper): remove unused method

--- libs/scrapper.py
+++ libs/scrapper.py
@@ -1,12 +1,2 @@
-import pandas as pd
-
-
 class Scrapper:
-    @classmethod
-    def scraps(cls, soups, scrap_fun):
-        data = pd.DataFrame()
-        for soup in soups:
-            dct = scrap_fun(soup)
-            if dct is not None:
-                data = data.append(dct)
-        return data
+    pass
